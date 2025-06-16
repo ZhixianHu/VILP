fb2db84445e2ed522636c712a7e5ecf8d920c3b7
Zhengtong Xu <68215739+ZhengtongXu@users.noreply.github.com>
2025-06-15 22:54:54 -0400

    Update install_custom_packages.sh

--- install_custom_packages.sh
+++ install_custom_packages.sh
@@ -9,6 +9,12 @@ mkdir third_party
 cd third_party
 git clone https://github.com/real-stanford/diffusion_policy.git
 cd diffusion_policy
+touch diffusion_policy/__init__.py
+pip install -e .
+cd ..
+git clone https://github.com/CompVis/latent-diffusion.git
+cd latent-diffusion
+touch ldm/__init__.py
 pip install -e .
 cd ..
 cd ..
